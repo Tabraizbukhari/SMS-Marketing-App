89e20920474e1171ae7ba9e8e61af3910883f223
Tabraiz <m.tabraizbukhari@gmail.com>
2021-01-02 18:33:42 +0500

    set dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,11 +13,25 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Bootstrap core CSS     -->
+        <link href="{{  asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" />
+        <!-- Animation library for notifications   -->
+        <link href="{{  asset('admin/css/animate.min.css') }}" rel="stylesheet"/>
+        <!--  Light Bootstrap Table core CSS    -->
+        <link href="{{  asset('admin/css/light-bootstrap-dashboard.css?v=1.4.0') }}" rel="stylesheet"/>
+        <!--  CSS for Demo Purpose, don't include it in your project     -->
+        <link href="{{  asset('admin/css/demo.css') }}" rel="stylesheet" />
+        <!--     Fonts and icons     -->
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+        <link href="{{ asset('admin/css/pe-icon-7-stroke.css')}}" rel="stylesheet" />
     </head>
     <body class="font-sans antialiased">
+        @include('dashboard.includes.navigation')
             {{ $header }}
         <main>
             {{ $slot }}
         </main>
     </body>
+        @include('dashboard.includes.script')
 </html>
