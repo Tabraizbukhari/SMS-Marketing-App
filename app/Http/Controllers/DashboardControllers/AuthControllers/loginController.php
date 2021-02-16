5590da72c7b2d0568393a4d358c7c6680a600c74
Tabraiz <m.tabraizbukhari@gmail.com>
2021-02-16 12:25:33 +0500

    login changes

--- app/Http/Controllers/DashboardControllers/AuthControllers/loginController.php
+++ app/Http/Controllers/DashboardControllers/AuthControllers/loginController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers\DashboardControllers\AuthControllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-        use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 class loginController extends Controller
 {
     public function login(Request $request)
     {
-        $email = $request->email;
         $password = $request->password;
-        if(Auth::attempt(['email' => $email, 'password' => $password])) {
+        $user = User::where('name', $request->username)->first();
+        if($user == null){
+            return redirect()->back()->withErrors('incorrect username!'); 
+        }
+        if(Auth::attempt(['email' => $user->email, 'password' => $password])) {
             return redirect()->route('admin.dashboard');
         }else{
             return redirect()->back()->withErrors('something wents wrong');
--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -5,6 +5,17 @@
 				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
 					<div class="d-table-cell align-middle">
 
+						@if($errors->any())
+							@foreach ($errors->all() as $error)
+								<div class="alert alert-danger alert-dismissible" role="alert">
+								<button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+								<div class="alert-message">
+									{{ $error }}
+								</div>
+								</div>
+							@endforeach
+						@endif
+						
 						<div class="text-center mt-4">
 							<h1 class="h2">Welcome back,</h1>
 							<p class="lead">
@@ -20,8 +31,8 @@
 									</div>
 									<form method="post" action="{{ route('login.post') }}"> @csrf
 										<div class="mb-3">
-											<label class="form-label">Email</label>
-											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email" />
+											<label class="form-label">Username</label>
+											<input class="form-control form-control-lg" type="text" name="username" placeholder="Enter your Username" />
 										</div>
 										<div class="mb-3">
 											<label class="form-label">Password</label>
