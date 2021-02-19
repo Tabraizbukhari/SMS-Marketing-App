e530fb3f05460607166fa7830732af8310dda43e
Tabraiz <m.tabraizbukhari@gmail.com>
2021-02-19 18:06:17 +0500

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
-
 require('./bootstrap');
 require('alpinejs');
 require('./backend.js');
 
 import $ from 'jquery';
 import select2 from 'select2';
+import InfiniteLoading from 'vue-infinite-loading';
+
+window.Vue = require('vue');
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const app = new Vue({
+    el: '#app',
+});
+
+Vue.use(InfiniteLoading);
