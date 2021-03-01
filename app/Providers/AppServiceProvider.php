8915492ff0835dca91f7947a02b47866c3715cd9
Tabraiz <m.tabraizbukhari@gmail.com>
2021-03-01 16:30:43 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Channels\DatabaseChannel;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Notifications\Channels\DatabaseChannel as IlluminateDatabaseChannel;
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();
         $this->app->instance(IlluminateDatabaseChannel::class, new DatabaseChannel());
     }
 }
