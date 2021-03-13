47980d5f3b910d5fb8af832f55ce50ca72d1f68b
Tabraiz <m.tabraizbukhari@gmail.com>
2021-03-13 12:31:31 +0500

    Update index.blade.php

--- resources/views/dashboard/customer/index.blade.php
+++ resources/views/dashboard/customer/index.blade.php
@@ -83,7 +83,7 @@
                                   <div class="modal-dialog modal-dialog-centered" role="document">
                                     <div class="modal-content">
                                       <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Incoming API Prefix</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                           <span aria-hidden="true">&times;</span>
                                         </button>
@@ -96,7 +96,7 @@
                                         </div>
                                         <div class="modal-footer">
                                           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                          <button type="submit" class="btn btn-primary">Save changes</button>
+                                          <button type="submit" class="btn btn-success">Save changes</button>
                                         </div>
                                       </form>
                                     </div>
