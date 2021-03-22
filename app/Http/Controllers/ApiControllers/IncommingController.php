8e69a9db9fff20112c9fd8ba4f10833999b3b3ea
Tabraiz <m.tabraizbukhari@gmail.com>
2021-03-22 16:41:32 +0500

    Update IncommingController.php

--- app/Http/Controllers/ApiControllers/IncommingController.php
+++ app/Http/Controllers/ApiControllers/IncommingController.php
@@ -66,7 +66,7 @@ class IncommingController extends Controller
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);        
         $result=  curl_exec($ch);
-        dd($result);
+        dd($result, 'ssssssssss');
         $data = json_decode($result, true);
         if($data['status'] == true){
             return true;
