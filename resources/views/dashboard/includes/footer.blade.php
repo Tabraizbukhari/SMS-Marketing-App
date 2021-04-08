81d8d4186d7c54425e189a5aa8888ba688385891
Tabraiz <m.tabraizbukhari@gmail.com>
2021-04-08 05:19:47 +0500

    Update footer.blade.php

--- resources/views/dashboard/includes/footer.blade.php
+++ resources/views/dashboard/includes/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row text-muted">
             <div class="col-6 text-left">
                 <p class="mb-0">
-                    <a href="index.html" class="text-muted"><strong>Developed By Syntechsol</strong></a> &copy; 2021
+                    <a href="https://synctechsol.com" class="text-muted"><strong>Developed By Syntechsol</strong></a> &copy; 2021
                 </p>
             </div>
             <div class="col-6 text-right">
