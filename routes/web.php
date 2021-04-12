255fbd1da3fbf6524c2fc8c8d54e3d00dcd2bcbf
Tabraiz <m.tabraizbukhari@gmail.com>
2021-04-12 17:06:06 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use App\Http\Controllers\DashboardControllers\ContactController;
 |
 */
 
-Route::group(array('domain' => '{account}.synctechsol.com'), function() {
+Route::group(array('domain' => '{account}.sms1.synctechsol.com'), function() {
 
     Route::get('/', function($account) {
         return $account;
