25f9835e58c104bc359ff5d1d5d8d381542670eb
Tabraiz <m.tabraizbukhari@gmail.com>
2021-04-29 03:53:55 +0500

    Update registeration.blade.php

--- resources/views/emails/invoices/registeration.blade.php
+++ resources/views/emails/invoices/registeration.blade.php
@@ -3,14 +3,29 @@
 
 @component('mail::panel')
 <h1 class="text-center">Registeration Processing Invoice</h1>
-Hello {{ $user->full_name }},
+Hello {{ $user['first_name'] }},
+   
+
+@endcomponent
+
+@component('mail:table')
+    <tr> 
+        <th width="70%">Title</th>
+        <th>Amount</th>
+    </tr>
+    <tr>
+        <td width="70%"> Registeration Processing fees</td>
+        <td>200</td>
+    </tr>
+    <tr>
+        <td colspan="2">200$</td>
+    </tr>
 @endcomponent
 
 @component('mail::subcopy')
-{{$data['message']}}
 @endcomponent
 
 
 Thanks,<br>
-{{ config('app.name') }}
+#footer
 @endcomponent
