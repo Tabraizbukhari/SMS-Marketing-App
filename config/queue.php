46591e3c45169bd1074be28066f7e5ff7464a799
Tabraiz <m.tabraizbukhari@gmail.com>
2021-04-30 03:50:03 +0500

    Update queue.php

--- config/queue.php
+++ config/queue.php
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 90,
+            'retry_after' => 1000000,
         ],
 
         'beanstalkd' => [
@@ -63,7 +63,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => 90,
+            'retry_after' => 1000000,
             'block_for' => null,
         ],
 
